3ee7b5b4e1f140de09b3b0643e8a3de7e04657d1
hy <38907025+water135@users.noreply.github.com>
2018-09-21 18:32:18 +0800

    Update uaredirect.js

--- style/uaredirect.js
+++ style/uaredirect.js
@@ -1,17 +1,3 @@
 // JavaScript Document
 document.write("<audio src=\"img/天然呆-后弦·《我知道你也很想念》.mp3\" autoplay=\"autoplay\"></audio>");
 
-function uaredirect(murl){
-	try {
-		if(document.getElementById("bdmark") != null){
-		return;
-		}
-		var urlhash = window.location.hash;
-		if (!urlhash.match("fromapp")){
-			if ((navigator.userAgent.match(/(iPhone|iPod|Android|ios|iPad)/i))) {
-				location.replace(murl);
-			}
-		}
-	} 
-	catch(err){}
-}
